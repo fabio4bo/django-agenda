9ee25c4cfa34b326f3f31a6c2b8a94adf836d65d
Fábio Souza <zantoniofabio@gmail.com>
2024-11-27 11:11:40 -0500

    Single contact page

--- agenda_app/contact/views/contact_views.py
+++ agenda_app/contact/views/contact_views.py
@@ -6,12 +6,35 @@ from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
-    paginator = Paginator(contacts, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    context = {'contacts': contacts,}
 
-    context = {'page_obj': page_obj, 'site_title': 'Contatos - '}
+    print(contacts.query)
+
+    # paginator = Paginator(contacts, 10)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+
+    # context = {'page_obj': page_obj, 'site_title': 'Contatos - '}
 
     return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+    # get - único
+    # filter - queryset
+    # single_contact = Contact.objects.get(pk=contact_id)
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    # single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)  # AND
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
