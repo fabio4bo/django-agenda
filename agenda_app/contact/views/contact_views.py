88c9858e8645f8feb06120f82e29151099837bd4
Fábio Souza <zantoniofabio@gmail.com>
2024-11-27 11:31:40 -0500

    Site title

--- agenda_app/contact/views/contact_views.py
+++ agenda_app/contact/views/contact_views.py
@@ -8,7 +8,10 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
-    context = {'contacts': contacts,}
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+        }
 
     print(contacts.query)
 
@@ -29,8 +32,10 @@ def contact(request, contact_id):
     # single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))  # AND
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)  # AND
 
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(
