f564fcd21cdbc5f90213d42f627f81cf906cc6c7
Fábio Souza <zantoniofabio@gmail.com>
2024-11-27 11:12:17 -0500

    'Show' editable in Admin

--- agenda_app/contact/admin.py
+++ agenda_app/contact/admin.py
@@ -13,6 +13,7 @@ class ContactAdmin(admin.ModelAdmin):
         'first_name',
         'last_name',
         'phone',
+        'show',
     )
     ordering = ('-id',)
     # list_filter = ('created_date',)
@@ -23,7 +24,7 @@ class ContactAdmin(admin.ModelAdmin):
     )
     list_per_page = 10
     list_max_show_all = 100
-    list_editable = ('last_name',)
+    list_editable = ('show',)
     list_display_links = (
         'id',
         '__str__',
