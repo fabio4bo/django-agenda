298545d5e07a23c2a781f3af30f160dd6a484f8b
Fábio Souza <zantoniofabio@gmail.com>
2024-11-27 12:47:13 -0500

    CRUD urls

--- agenda_app/contact/urls.py
+++ agenda_app/contact/urls.py
@@ -5,7 +5,14 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
+    # path('<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+
+    # contact CRUD
+    # path('contact/create/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
