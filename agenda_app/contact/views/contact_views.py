402101d1a8fbd96d8a1df0b0e006a2fd49914b41
Fábio Souza <zantoniofabio@gmail.com>
2024-11-27 12:27:30 -0500

    Pagination

--- agenda_app/contact/views/contact_views.py
+++ agenda_app/contact/views/contact_views.py
@@ -6,18 +6,19 @@ from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
 
-    context = {'contacts': contacts, 'site_title': 'Contatos - '}
 
-    print(contacts.query)
+    # print(contacts.query)
 
-    # paginator = Paginator(contacts, 10)
-    # page_number = request.GET.get('page')
-    # page_obj = paginator.get_page(page_number)
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     # context = {'page_obj': page_obj, 'site_title': 'Contatos - '}
 
+    context = {'page_obj': page_obj, 'site_title': 'Contatos - '}
+    
     return render(request, 'contact/index.html', context)
 
 
