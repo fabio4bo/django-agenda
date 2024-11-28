f9d1d82fe736173175404e2a36842eae7fc96b9d
Fábio Souza <zantoniofabio@gmail.com>
2024-11-28 12:23:47 -0500

    ContactForm -> forms.py

---
+++ agenda_app/contact/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone',
+        )
+
+    def clean(self):  # access to the data before saving.
+        # cleaned_data = self.cleaned_data
+        # print(cleaned_data)
+
+        self.add_error('first_name', ValidationError('Error Message', code='invalid'))
+        self.add_error(
+            None, ValidationError('Error Message', code='invalid')  # non_field_errors
+        )
+        return super().clean()
--- agenda_app/contact/views/contact_forms.py
+++ agenda_app/contact/views/contact_forms.py
@@ -1,50 +1,12 @@
-from django.core.paginator import Paginator
-from django.core.exceptions import ValidationError
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import render
 
-from contact.models import Contact
-
-from django import forms
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name',
-            'last_name',
-            'phone',
-        )
-    def clean(self):  # access to the data before saving.
-        cleaned_data = self.cleaned_data
-        # print(cleaned_data)
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Error Message',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            None,  # non_field_errors
-            ValidationError(
-                'Error Message',
-                code='invalid'
-            )
-        )
-        return super().clean()
+from contact.forms import ContactForm
 
 
 def create(request):
     if request.method == 'POST':
-        context = {
-            'form': ContactForm(request.POST)
-        }
+        context = {'form': ContactForm(request.POST)}
     else:
-        context = {
-            'form': ContactForm()  # empty
-        }
+        context = {'form': ContactForm()}  # empty; GET
     print(request.method)
     return render(request, 'contact/create.html', context)
