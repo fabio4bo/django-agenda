0373e456ffa6a6196d3d7ca7e1cbc7df53337dc9
Fábio Souza <zantoniofabio@gmail.com>
2024-11-28 10:48:54 -0500

    Dinamic form

--- agenda_app/contact/views/contact_forms.py
+++ agenda_app/contact/views/contact_forms.py
@@ -4,7 +4,27 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
+from django import forms
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone',
+        )
+
 
 def create(request):
-    context = {}
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+    else:
+        context = {
+            'form': ContactForm()  # empty
+        }
+    print(request.method)
     return render(request, 'contact/create.html', context)
