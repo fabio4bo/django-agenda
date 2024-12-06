0551078463fb0e134a5768c9b9dc9827687b8713
Fábio Souza <zantoniofabio@gmail.com>
2024-12-06 10:22:38 -0500

    Menus for authenticated user

--- agenda_app/contact/urls.py
+++ agenda_app/contact/urls.py
@@ -6,9 +6,9 @@ app_name = 'contact'
 urlpatterns = [
     # path('<int:contact_id>/', views.contact, name='contact'),
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
-    path('contact/<int:contact_id>/update/', views.update, name='update'),
-    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
-    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),  # logged in and owner
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),  # logged in and owner
+    path('contact/create/', views.create, name='create'),  # logged in
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
     # contact CRUD
@@ -17,6 +17,6 @@ urlpatterns = [
     # user
     path('user/create/', views.register, name='register'),
     path('user/login/', views.login_view, name='login'),
-    path('user/logout/', views.logout_view, name='logout'),
-    path('user/update/', views.user_update, name='user_update'),
+    path('user/logout/', views.logout_view, name='logout'),  # logged in
+    path('user/update/', views.user_update, name='user_update'),  # logged in
 ]
