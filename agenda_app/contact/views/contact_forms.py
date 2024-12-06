4d710b1a2dd1584403c365f995475ba9e5050084
Fábio Souza <zantoniofabio@gmail.com>
2024-12-06 10:37:08 -0500

    @login_required

--- agenda_app/contact/views/contact_forms.py
+++ agenda_app/contact/views/contact_forms.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
-
 from contact.forms import ContactForm
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -27,6 +28,7 @@ def create(request):
     return render(request, 'contact/create.html', context)
 
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
@@ -54,6 +56,7 @@ def update(request, contact_id):
     return render(request, 'contact/create.html', context)
 
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
